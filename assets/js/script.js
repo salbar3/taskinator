72613c24cb320593daf2cfe53cf5e6270f43b4e5
Salvador Barragan <salvadorbarragan@Salvadors-MacBook-Pro.local>
2022-03-03 11:40:13 -0800

    4.2.5 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,18 @@ var buttonEl = document.querySelector("#save-task");
 
 var tasksToDoEl = document.querySelector("#tasks-to-do")
 
+var formEl = document.querySelector("#task-form")
+
 var createTaskHandler = function() {
+
+    event.preventDefault()
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+       
 }
 
-buttonEl.addEventListener('click', createTaskHandler)
+formEl.addEventListener('submit', createTaskHandler)
 
