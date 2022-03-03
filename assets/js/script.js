9daf4df087f3ebd2e88d8071c8b646a7237a039c
Salvador Barragan <salvadorbarragan@Salvadors-MacBook-Pro.local>
2022-03-03 12:32:23 -0800

    4.2.6 complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,22 @@ var createTaskHandler = function() {
 
     event.preventDefault()
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value
+
+    var taskTypeInput = document.querySelector("select[name= 'task-type']").value
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-       
+
+    var taskInfoEl = document.createElement('div')
+    taskInfoEl.className = 'task-info'
+    
+    
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+
+    tasksToDoEl.appendChild(listItemEl)
+
 }
 
 formEl.addEventListener('submit', createTaskHandler)
