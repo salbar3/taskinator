1f5057e1a138806c33e3b29479e94c88da31b7c2
Salvador Barragan <salvadorbarragan@Salvadors-MacBook-Pro.local>
2022-03-03 13:58:55 -0800

    4.2.7 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do")
 
 var formEl = document.querySelector("#task-form")
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
 
     event.preventDefault()
 
@@ -12,19 +12,28 @@ var createTaskHandler = function() {
 
     var taskTypeInput = document.querySelector("select[name= 'task-type']").value
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+
+    createTaskEl(taskDataObj)
+
+}
+
+var createTaskEl = function(taskDataObj) {
+   
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement('div')
     taskInfoEl.className = 'task-info'
+    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskDataObj.name + "</h3><span class= 'task-type'>" + taskDataObj.type + "</span>";
     
-    
-    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl)
 
     tasksToDoEl.appendChild(listItemEl)
-
 }
 
-formEl.addEventListener('submit', createTaskHandler)
+formEl.addEventListener('submit', taskFormHandler)
 
