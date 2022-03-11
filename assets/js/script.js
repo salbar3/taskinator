d3ad843eb288be9a8757a670d61b79111ec79c17
Salvador Barragan <salvadorbarragan@Salvadors-MacBook-Pro.local>
2022-03-10 20:54:18 -0800

    left off at 4.4.4, just finished adding updated status to the tasks object

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
 var pageContentEl = document.querySelector("#page-content");
 
+var tasks = [];
+
 var taskFormHandler = function(event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -30,7 +32,8 @@ var taskFormHandler = function(event) {
   } else {
     var taskDataObj = {
       name: taskNameInput,
-      type: taskTypeInput
+      type: taskTypeInput,
+      status: "to do"
     };
 
     createTaskEl(taskDataObj);
@@ -52,8 +55,13 @@ var createTaskEl = function(taskDataObj) {
   listItemEl.appendChild(taskActionsEl);
   tasksToDoEl.appendChild(listItemEl);
 
+  taskDataObj.id = taskIdCounter
+  tasks.push(taskDataObj)
+
   // increase task counter for next unique id
   taskIdCounter++;
+  console.log(taskDataObj)
+  console.log(taskDataObj.status)
 };
 
 var createTaskActions = function(taskId) {
@@ -103,6 +111,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
 
+ // loop through tasks array and taks object with the new content
+  for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id === parseInt(taskId)) {
+          tasks[i].name = taskName;
+          tasks[i].type = taskType;
+      }
+  }
+
   alert("Task Updated!");
 
   // remove data attribute from form
@@ -144,6 +160,14 @@ var taskStatusChangeHandler = function(event) {
   } else if (statusValue === "completed") {
     tasksCompletedEl.appendChild(taskSelected);
   }
+
+  for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id === parseInt(taskId)){
+          tasks[i].id = statusValue
+      }
+  }
+
+  console.log(tasks)
 };
 
 var editTask = function(taskId) {
